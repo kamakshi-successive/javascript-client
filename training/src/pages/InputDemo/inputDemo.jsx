9f6350c88e24acb5510e7ce77b74a75c72f50c6a
kamakshikumari <kamakshi.kumari@successive.tech>
2020-12-13 16:07:36 +0530

    Controlled Inputs

---
+++ training/src/pages/InputDemo/inputDemo.jsx
@@ -0,0 +1,86 @@
+/* eslint-disable no-console */
+import React from 'react';
+import {
+  TextField, SelectField, RadioField,
+} from '../../components';
+import {
+  // eslint-disable-next-line no-unused-vars
+  selectOptions, radioOptionsCricket, radioOptionsFootball,
+} from '../../config/constant';
+
+class InputDemo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      sport: '',
+      cricket: '',
+      football: '',
+    };
+  }
+
+handleNameChange = (e) => {
+  this.setState({ name: e.target.value }, () => {
+    console.log(this.state);
+  });
+}
+
+handleSportChange = (e) => {
+  this.setState({ sport: e.target.value }, () => console.log(this.state));
+  if (e.target.value === 'Select') {
+    this.setState({ sport: '' });
+  }
+  return e.target.value === 'cricket' ? this.setState({ football: e.target.value }) : this.setState({ cricket: e.target.value });
+}
+
+handlePositionChange = (e) => {
+  const { sport } = this.state;
+  return sport === 'cricket' ? this.setState({ cricket: e.target.value }, () => console.log(this.state)) : this.setState({ football: e.target.value }, () => console.log(this.state));
+}
+
+RadioOption = () => {
+  let { radioValue } = this.state;
+  const { sport } = this.state;
+  if (sport === 'cricket') {
+    radioValue = radioOptionsCricket;
+  } else if (sport === 'football') {
+    radioValue = radioOptionsFootball;
+  }
+  return (radioValue);
+};
+
+render() {
+  const { sport } = this.state;
+  return (
+    <>
+      <div>
+        <p><b>Name: </b></p>
+        <TextField error="" onChange={this.handleNameChange} />
+        <p><b>Select the game you Play?</b></p>
+        <SelectField
+          error=""
+          onChange={this.handleSportChange}
+          options={selectOptions}
+          defaultText="Select"
+        />
+        <div>
+          {
+            (sport === '' || sport === 'Select') ? ''
+              : (
+                <>
+                  <p><b>What you do?</b></p>
+                  <RadioField
+                    error=""
+                    options={this.RadioOption()}
+                    onChange={this.handlePositionChange}
+                  />
+                </>
+              )
+          }
+        </div>
+      </div>
+    </>
+  );
+}
+}
+export default InputDemo;
