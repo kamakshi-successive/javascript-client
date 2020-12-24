1faecc271b22d5688a025853c696d13135dcf093
kamakshikumari <kamakshi.kumari@successive.tech>
2020-12-24 10:12:23 +0530

    Processing PR

---
+++ src/pages/Trainee/TraineeList.jsx
@@ -0,0 +1,109 @@
+/* eslint-disable no-console */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, withStyles } from '@material-ui/core';
+import moment from 'moment';
+import { AddDialog } from './components/AddDialog';
+import trainees from './data/trainee';
+import { Table1 } from '../../components';
+
+const useStyles = (theme) => ({
+  root: {
+    margin: theme.spacing(2),
+  },
+  dialog: {
+    textAlign: 'right',
+  },
+});
+
+class TraineeList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      orderBy: '',
+      order: '',
+    };
+  }
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    const { open } = this.state;
+    this.setState({ open: false });
+    return open;
+  };
+
+  handleSubmit = (data) => {
+    this.settate({
+      open: false,
+    }, () => console.log(data));
+  }
+
+  handleSort = (field) => (event) => {
+    const { order } = this.state;
+    console.log(event);
+    this.setState({
+      orderBy: field,
+      order: order === 'asc' ? 'desc' : 'asc',
+    });
+  }
+
+  handleSelect = (event) => {
+    console.log(event);
+  }
+
+  // getDateFormatted = (date) => {
+  //   moment(date).format('dddd, MMMM Do YYYY, h:mm:ss a');
+  // };
+
+  render() {
+    const { open, orderBy, order } = this.state;
+    const { classes } = this.props;
+    return (
+      <>
+        <div className={classes.root}>
+          <div className={classes.dialog}>
+            <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
+              ADD TRAINEELIST
+            </Button>
+            <AddDialog open={open} onClose={this.handleClose} onSubmit={() => this.handleSubmit} />
+          </div>
+          <Table1
+            id="id"
+            data={trainees}
+            column={[
+              {
+                field: 'name',
+                label: 'Name',
+                align: 'center',
+              },
+              {
+                field: 'email',
+                label: 'Email Address',
+                format: (value) => value && value.toUpperCase(),
+              },
+              {
+                field: 'createdAt',
+                label: 'Date',
+                align: 'right',
+                format: this.getDateFormatted,
+              },
+
+            ]}
+            orderBy={orderBy}
+            order={order}
+            onSort={this.handleSort}
+            onSelect={this.handleSelect}
+          />
+        </div>
+      </>
+    );
+  }
+}
+TraineeList.propTypes = {
+  classes: PropTypes.objectOf(PropTypes.string).isRequired,
+};
+export default withStyles(useStyles)(TraineeList);
